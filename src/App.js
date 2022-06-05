9c4eb1c8adf640e3ec56a5eadbf925e750541417
aroenvr <aroenvr@gmail.com>
2022-06-05 22:18:40 +0200

    Trying to make horeko work with Web3

--- src/App.js
+++ src/App.js
@@ -33,6 +33,18 @@ function App() {
           const currentAddress = await web3Provider.getSigner().provider.provider.selectedAddress;
 
           setAddress(currentAddress);
+
+          let web3;
+          if (window.ethereum) {
+            console.log("window.ethereum");
+            // web3 = new Web3(window.ethereum);
+            web3 = new ethers.providers.Web3Provider(window.ethereum);
+          } else if (window.web3) {
+            console.log("window.web3");
+            // web3 = new Web3(window.web3.currentProvider);
+            web3 = new ethers.providers.Web3Provider(window.web3);
+          };
+          console.log(web3);
         }}      
       >
         Update Address
